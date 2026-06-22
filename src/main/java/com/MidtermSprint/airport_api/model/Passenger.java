58a0b5ed654dfb411fb1ad35c7d9925269409e66
BCoishous <Brandon.Coish@gmail.com>
2026-06-22 19:46:40 -0230

    Add Passenger entity, repository, and controller

---
+++ src/main/java/com/MidtermSprint/airport_api/model/Passenger.java
@@ -0,0 +1,41 @@
+package com.MidtermSprint.airport_api.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "passengers")
+public class Passenger {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "city_id")
+    private City city;
+
+    public Passenger() {}
+
+    public Passenger(String firstName, String lastName, String phoneNumber, City city) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.city = city;
+    }
+
+    public Long getId() { return id; }
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public String getPhoneNumber() { return phoneNumber; }
+    public City getCity() { return city; }
+
+    public void setId(Long id) { this.id = id; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+    public void setCity(City city) { this.city = city; }
+}
